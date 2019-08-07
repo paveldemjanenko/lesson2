17dd94b2444c748fe28981024fd4fca8b7100c26
paveldemjanenko <pavel.demjanenko@gmail.com>
2019-08-07 16:42:34 +0300

    init

---
+++ controllers/HomeController.js
@@ -0,0 +1,34 @@
+import AppError from '../errors/AppError';
+import mysql from 'mysql';
+
+const logger = require('../utils/logger')('HomeController');
+
+const indexAction = async (req, res, next) => {
+  logger.log('info', `healthCheck: ${JSON.stringify(req.params)}`);
+
+  try {
+    const connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
+
+    connection.connect();
+
+    connection.query('SELECT * from user', null, (error, results, fields) => {
+      if (error) {
+        console.log(error);
+      }
+
+      if (results) {
+        res.json(results);
+      }
+    });
+
+  } catch (err) {
+    next(new AppError(err.message, 400));
+  }
+};
+
+export default indexAction;
